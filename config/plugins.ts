18145dd1fadd604ad9182023bb41d3aa8bc10d6c
SAMYEH <42162992+samyeh621109@users.noreply.github.com>
2025-05-09 14:00:26 +0800

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default () => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: process.env.JWT_SECRET || 'hM9t0XgTr3Y5p8XnB2f5Ug==',
+    },
+  },
+});
